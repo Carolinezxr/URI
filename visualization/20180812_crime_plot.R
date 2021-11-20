ceaf99d48c9e84e73998b6d53d9c33994197bc3a
XRZeng <50905009+Carolinezxr@users.noreply.github.com>
2021-11-20 11:14:28 -0600

    bar chart, circular bar chart

--- visualization/20180812_crime_plot.R
+++ visualization/20180812_crime_plot.R
@@ -7,21 +7,6 @@ library(dplyr)
 
 library(date)
 
-trans<-function(x){
-  a<-aggregate(x$`Primary Type`,by=list(day=x$day, type=x$`Primary Type`),length)
-  
-  type_ts2<-as.data.frame(unique(a$day))
-  colnames(type_ts2)<-"day"
-  for (i in 1:length(unique(a$type))){
-    b<-subset(a[which(a$type==unique(a$type)[i]),])
-    type_ts2<-merge(type_ts2,b[,c(1,3)],by="day",all=TRUE)
-    colnames(type_ts2)[(i+1)]<-unique(a$type)[i]
-  }
-  
-  b<-type_ts2
-  rownames(b)<-b$day
-  b<-b[,-1]
-}
 
 crimes_data<-read_csv("E:\\data\\project\\crimes_data.csv",)
 crimes_data$day<-as.Date(unlist(crimes_data$Date),"%m/%d/%Y")
@@ -40,12 +25,7 @@ raw_crimes<-raw_crimes[,c(2,1,3)]
 
 colnames(raw_crimes)=c('hour',"type","value")
 
-ggplot(raw_crimes,mapping = aes(hour,value,fill=type))+
-  geom_bar(stat='identity') +
-  labs(x = 'hour',y = 'num_reports') +
-  theme(axis.title =element_text(size = 16),axis.text =element_text(size = 14, color = 'black'))+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  theme_bw()
+
 
 raw_crimes1<-raw_crimes[1:16,]
 ggplot(raw_crimes1, ,mapping = aes(hour,value,fill=type)) +
@@ -54,3 +34,46 @@ ggplot(raw_crimes1, ,mapping = aes(hour,value,fill=type)) +
   scale_fill_brewer() +
   coord_polar(start=0) +
   scale_x_continuous("", limits = c(0, 12), breaks = seq(0, 12), labels = seq(0,12))
+
+my_data <- read.table("E:\\data\\counts.txt")
+my_data<-cbind(as.data.frame(as.numeric(rownames(my_data))-1),my_data)
+colnames(my_data)<-c("hour","Cluster1","Cluster2","Cluster3")               
+library(reshape2)
+plot<-melt(my_data,id = "hour")
+
+
+crime<-ggplot(raw_crimes,mapping = aes(hour,value,fill=type))+
+  geom_bar(stat='identity') +
+  labs(x = 'hour',y = 'num_reports') +
+  theme_bw()
+
+
+cluster<-ggplot(plot,mapping = aes(hour,value,fill=variable))+
+  geom_bar(stat='identity') +
+  labs(x = 'hour',y = 'clusters') +
+  theme_bw() 
+
+library(ggpubr)
+ggarrange(crime, cluster, 
+          labels = c("crime", "teo"),
+          ncol = 1, nrow = 2, align = "v")
+
+
+
+
+cluster<-ggplot(plot,mapping = aes(hour,value,fill=variable))+
+  geom_bar(stat='identity') +  
+  theme_minimal() +
+  coord_polar(start=0) +
+  scale_x_continuous("", limits = c(0, 24), breaks = seq(0, 24), labels = seq(0,24))
+
+crime<-ggplot(raw_crimes,mapping = aes(hour,value,fill=type))+
+  geom_bar(stat='identity') +  
+  theme_minimal() +
+  coord_polar(start=0) +
+  scale_x_continuous("", limits = c(0, 24), breaks = seq(0, 24), labels = seq(0,24))
+
+ggarrange(crime, cluster, 
+          labels = c("crime", "teo"),
+          ncol = 1, nrow = 2, align = "v")
+
