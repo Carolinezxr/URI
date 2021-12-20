a5aa757141381a30b0a2d2a2d990965d6a60531c
XRZeng <50905009+Carolinezxr@users.noreply.github.com>
2021-12-20 13:05:33 -0600

    Update Align zone and district.R

--- reports/Align zone and district.R
+++ reports/Align zone and district.R
@@ -1,17 +1,27 @@
+library(readr)
+
+library(lubridate)
+
+library(dplyr)
+
 crimes_data<-read_csv("E://data//project//crimes_data.csv",)
 crimes_data$day<-date(as.Date(unlist(crimes_data$Date),"%m/%d/%Y"))
 crimes_data$Zone<-NA
-crimes_data$Zone[crimes_data$District==16 |crimes_data$District==17] <- 1
-crimes_data$Zone[crimes_data$District==19] <- 2
-crimes_data$Zone[crimes_data$District==12 |crimes_data$District==14] <- 3
-crimes_data$Zone[crimes_data$District==1 |crimes_data$District==18] <- 4
-crimes_data$Zone[crimes_data$District==2] <- 5
-crimes_data$Zone[crimes_data$District==7 |crimes_data$District==8] <- 6
-crimes_data$Zone[crimes_data$District==3] <- 7
-crimes_data$Zone[crimes_data$District==4 |crimes_data$District==6] <- 8
-crimes_data$Zone[crimes_data$District==5 |crimes_data$District==22] <- 9
-crimes_data$Zone[crimes_data$District==10 |crimes_data$District==11] <- 10
-crimes_data$Zone[crimes_data$District==20 |crimes_data$District==24] <- 11
-crimes_data$Zone[crimes_data$District==15 |crimes_data$District==25] <- 12
-crimes_data$Zone[crimes_data$District==9] <- 13
-#write.csv(crimes_data,"E://data//project//crimes_data_zone.csv")
+unique(crimes_data$District)
+crimes_data$Zone[crimes_data$District=="016" |crimes_data$District=="017"] <- 1
+crimes_data$Zone[crimes_data$District=="019"] <- 2
+crimes_data$Zone[crimes_data$District=="012" |crimes_data$District=="014"] <- 3
+crimes_data$Zone[crimes_data$District=="001" |crimes_data$District=="018"] <- 4
+crimes_data$Zone[crimes_data$District=="002"] <- 5
+crimes_data$Zone[crimes_data$District=="007" |crimes_data$District=="008"] <- 6
+crimes_data$Zone[crimes_data$District=="003"] <- 7
+crimes_data$Zone[crimes_data$District=="004" |crimes_data$District=="006"] <- 8
+crimes_data$Zone[crimes_data$District=="005" |crimes_data$District=="022"] <- 9
+crimes_data$Zone[crimes_data$District=="010" |crimes_data$District=="011"] <- 10
+crimes_data$Zone[crimes_data$District=="020" |crimes_data$District=="024"] <- 11
+crimes_data$Zone[crimes_data$District=="015" |crimes_data$District=="025"] <- 12
+crimes_data$Zone[crimes_data$District=="009"] <- 13
+a<-crimes_data[is.na(crimes_data$Zone),c("District","Zone")]
+b<-crimes_data[!is.na(crimes_data$Zone),]
+
+write.csv(b,"E://data//project//crimes_data_zone.csv")
